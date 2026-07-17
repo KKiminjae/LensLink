abb86f9e6f2b469f5a9247896f4b41ee769ad5a7
KKiminjae <jason471566@gmail.com>
2026-07-17 16:07:45 +0900

    feat: separate search results into new and used products

--- src/main/java/com/lenslink/domain/search/dto/SearchResponse.java
+++ src/main/java/com/lenslink/domain/search/dto/SearchResponse.java
@@ -1,4 +1,16 @@
 package com.lenslink.domain.search.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
 public class SearchResponse {
+    private AnalyzeResponse analysis;
+
+    private List<ProductResponse> newProducts;
+
+    private List<ProductResponse> usedProducts;
 }
